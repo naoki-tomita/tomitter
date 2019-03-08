da0883802dddf7bb58c7a56face69183d48f3848
naoki.tomita <mxcpy608@hotmail.com>
2019-03-08 21:23:41 +0900

    WIP

--- e2e/users/src/main.rs
+++ e2e/users/src/main.rs
@@ -2,6 +2,12 @@ extern crate reqwest;
 
 use std::collections::HashMap;
 
+fn createRequest() {
+    let client = reqwest::Client::new();
+    let response = client.post("http://localhost:8080/users")
+        .body(CreateParameter { loginName: "foo".to_owned(), password: "bar".to_owned() });
+}
+
 fn main() -> Result<(), Box<std::error::Error>> {
     let resp: HashMap<String, String> = reqwest::get("https://httpbin.org/ip")?
         .json()?;
@@ -9,7 +15,17 @@ fn main() -> Result<(), Box<std::error::Error>> {
     Ok(())
 }
 
-struct CreateRequest {
+struct CreateParameter {
     loginName: String,
     password: String
+}
+
+struct LoginParameter {
+    loginName: String,
+    password: String
+}
+
+struct User {
+    id: i32,
+    loginName: String
 }
