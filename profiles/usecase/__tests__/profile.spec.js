dfff4c1c4aaae4c504c321d805e6123ca2f4cc50
naoki.tomita <mxcpy608@hotmail.com>
2019-03-20 09:44:14 +0900

    Fix test.

--- profiles/usecase/__tests__/profile.spec.js
+++ profiles/usecase/__tests__/profile.spec.js
@@ -1,5 +1,6 @@
 jest.mock("../../gateway/profiles");
 jest.mock("../../gateway/users");
+const { when } = require("jest-when");
 const { User } = require("../../domain/users");
 const { Profile, Profiles } = require("../../domain/profiles");
 const { getIdentifiedUser } = require("../../gateway/users");
@@ -9,9 +10,14 @@ const { create, findByCookie } = require("../profile");
 describe("usecase test.", () => {
   describe("#create", () => {
     it("should create", async () => {
+      const cookie = "cookie";
       const profile = new Profile(1, 1, "foo", "bar");
+      const user = new User(1, "name");
+
+      when(getIdentifiedUser).calledWith(cookie).mockReturnValue(user);
       createProfile.mockReturnValue(profile);
-      expect(await create(1, "foo", "bar"));
+
+      expect(await create(cookie, "foo", "bar"));
       expect(createProfile).toBeCalledWith(new Profile(null, 1, "foo", "bar"));
     });
   });
@@ -27,7 +33,7 @@ describe("usecase test.", () => {
 
       expect(await findByCookie(cookie)).toEqual(profile);
       expect(getIdentifiedUser).toBeCalledWith(cookie);
-      expect(findByUserId).toBeCalledWith(user);
+      expect(findByUserId).toBeCalledWith(0);
     });
   });
 });
