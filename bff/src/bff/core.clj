11c88911ab15158353abae6baa45331d421c6816
naoki.tomita <mxcpy608@hotmail.com>
2019-07-25 13:23:36 +0900

    Add bff.

---
+++ bff/src/bff/core.clj
@@ -0,0 +1,6 @@
+(ns bff.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ bff/test/bff/core_test.clj
@@ -0,0 +1,7 @@
+(ns bff.core-test
+  (:require [clojure.test :refer :all]
+            [bff.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
