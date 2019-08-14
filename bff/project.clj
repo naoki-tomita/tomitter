e3487de9f44fbaba1b514ad4a3faf12a724e338b
kojiro.ueda <mxcpy608@hotmail.com>
2019-08-14 15:50:37 +0900

    Fix project.cli

--- bff/project.clj
+++ bff/project.clj
@@ -1,7 +1,7 @@
 (defproject bff "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/naoki-tomita"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :repl-options {:init-ns bff.core})
